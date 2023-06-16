82828b8e4e5dbb77b9e6ee053ee33e95825c834f
Rynhardt Engelbrecht <107496937+rynhardt-engelbrecht@users.noreply.github.com>
2023-06-16 13:14:11 +0200

    Add instance variables to keep track of players

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,4 +1,5 @@
 require_relative 'logic'
+require_relative 'player'
 
 class ConnectFour
   attr_accessor :grid, :current_turn, :next_turn
@@ -7,5 +8,9 @@ class ConnectFour
 
   def initialize
     @grid = Array.new(7) { Array.new(7, 0) }
+    @players = { player_one: Player.new, player_two: Player.new }
+    @current_turn = @players[:player_one]
+    @next_turn = @players[:player_two]
+    @is_active = true
   end
 end
