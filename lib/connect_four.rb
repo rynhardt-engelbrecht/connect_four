c8dc128f24fc2de548b84f15a7584085ddf27051
Rynhardt Engelbrecht <107496937+rynhardt-engelbrecht@users.noreply.github.com>
2023-06-18 13:05:56 +0200

    Simplify ConnectFour initialize method

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -7,30 +7,18 @@ require_relative 'instructions'
 
 # class representing a Connect Four game
 class ConnectFour
+  GRID = Array.new(7) { Array.new(7, 0) }
+
   attr_accessor :grid, :current_turn, :next_turn
 
   include CFLogic
   include Display
   include Instructions
 
-  def initialize
-    @grid = Array.new(7) { Array.new(7, 0) }
-    create_players
-    self.count_down
-    @current_turn = @players[:player_one]
-    @next_turn = @players[:player_two]
-    @is_active = true
-  end
-
-  def create_players
-    puts instructions
-    @players = Hash.new
-    puts pick_color('one')
-    @players[:player_one] = Player.new
-    puts "Player one, your color is: #{@players[:player_one].color}"
-    puts ''
-    puts pick_color('two')
-    @players[:player_two] = Player.new
-    puts "Player two, your color is: #{@players[:player_two].color}"
+  def initialize(grid = GRID, current_turn = Player.new, next_turn = Player.new, is_active: true)
+    @grid = grid
+    @current_turn = current_turn
+    @next_turn = next_turn
+    @is_active = is_active
   end
 end
