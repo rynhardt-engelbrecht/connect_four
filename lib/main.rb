08418ea809e59cbd1006ad02f57e61c43fbe2ef9
Rynhardt Engelbrecht <107496937+rynhardt-engelbrecht@users.noreply.github.com>
2023-06-18 13:08:07 +0200

    Add script to start the game

--- lib/main.rb
+++ lib/main.rb
@@ -1,4 +1,10 @@
-require_relative 'connect_four'
+# frozen_string_literal: true
 
+require_relative 'connect_four'
+require_relative 'instructions'
+
+include Instructions
+
+puts Instructions.instructions
 game = ConnectFour.new
 game.play_game
